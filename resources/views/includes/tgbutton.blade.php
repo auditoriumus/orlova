9ea9125d65a9d48c4426e470d777a417a76865fc
Юрий Сад <urij@MacBook-Pro-Urij.local>
2022-04-23 12:16:58 +0500

    tg button

---
+++ resources/views/includes/tgbutton.blade.php
@@ -0,0 +1,14 @@
+<style>
+    .telegram {
+        position: fixed; /* Фиксированное положение */
+        right: 5%; /* Расстояние от правого края окна браузера */
+        bottom: 5%; /* Расстояние сверху */
+        z-index: 1;
+    }
+</style>
+<div class="telegram">
+    <a href="https://telegram.me/shutova_iii/">
+        <img src="{{asset('assets/tg.png')}}" height="50px" alt="">
+    </a>
+</div>
+
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- animate CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
@@ -28,6 +29,7 @@
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
 <body>
+@include('includes.tgbutton')
 @include('includes.nav')
 @include('includes.errors')
 @include('includes.message')
