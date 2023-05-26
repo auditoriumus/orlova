e5bdc6d5c21a2a7cc6aa5f9a79fd41f8b29db76c
Юрий Сад <urij@MacBook-Pro-Urij.local>
2023-05-26 10:01:57 +0300

    закрыть кнопку купить

--- resources/views/pages/card.blade.php
+++ resources/views/pages/card.blade.php
@@ -89,48 +89,48 @@
                                 </div>
                             @endif
                         @else
-                            <form action="{{ route('order') }}" method="post">
-                                @csrf
-                                <div class="card_area">
-                                    <div class="product_count_area">
-                                        <p>{{ $course->price }} руб.</p>
-                                    </div>
-                                    <input type="number" name="price" hidden value="{{$course->price}}">
-                                    <input type="text" name="course_id" hidden value="{{$course->uuid}}">
-                                    <input type="text" name="description" hidden
-                                           value="Оплата за практику: {{$course->title}}">
-                                    <div class="mt-10">
-                                        <input type="text" name="email" placeholder="Email*" required
-                                               class="single-input"
-                                               @if(Auth::check()) value="{{\Illuminate\Support\Facades\Auth::user()->email}}" @endif>
-                                    </div>
-                                    <div class="mt-10">
-                                        <input type="text" name="phone" placeholder="Телефон*" required
-                                               class="single-input"
-                                               @if(Auth::check()) value="{{\Illuminate\Support\Facades\Auth::user()->phone}}" @endif>
-                                    </div>
-                                    <div class="mt-10">
-                                        <input type="text" name="name" placeholder="Имя" required class="single-input"
-                                               @if(Auth::check()) value="{{\Illuminate\Support\Facades\Auth::user()->name}}" @endif >
-                                    </div>
-                                    <div class="mt-10">
-                                        <input type="text" name="promo_code" placeholder="Промо-код" class="single-input">
-                                    </div>
-                                    <div class="mt-10 left">
-                                        <input type="checkbox" id="public_offer" name="public_offer" required>
-                                        <label for="public_offer">Подтверждаю согласие с <a
-                                                href="{{route('public-offer')}}">Договором публичной оферты</a></label>
-                                    </div>
-                                    <div class="mt-10">
-                                        <input type="checkbox" id="privacy_policy" name="privacy_policy" required>
-                                        <label for="privacy_policy">Подтверждаю согласие на обработку <a
-                                                href="{{route('privacy')}}">персональных данных</a></label>
-                                    </div>
+{{--                            <form action="{{ route('order') }}" method="post">--}}
+{{--                                @csrf--}}
+{{--                                <div class="card_area">--}}
+{{--                                    <div class="product_count_area">--}}
+{{--                                        <p>{{ $course->price }} руб.</p>--}}
+{{--                                    </div>--}}
+{{--                                    <input type="number" name="price" hidden value="{{$course->price}}">--}}
+{{--                                    <input type="text" name="course_id" hidden value="{{$course->uuid}}">--}}
+{{--                                    <input type="text" name="description" hidden--}}
+{{--                                           value="Оплата за практику: {{$course->title}}">--}}
+{{--                                    <div class="mt-10">--}}
+{{--                                        <input type="text" name="email" placeholder="Email*" required--}}
+{{--                                               class="single-input"--}}
+{{--                                               @if(Auth::check()) value="{{\Illuminate\Support\Facades\Auth::user()->email}}" @endif>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mt-10">--}}
+{{--                                        <input type="text" name="phone" placeholder="Телефон*" required--}}
+{{--                                               class="single-input"--}}
+{{--                                               @if(Auth::check()) value="{{\Illuminate\Support\Facades\Auth::user()->phone}}" @endif>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mt-10">--}}
+{{--                                        <input type="text" name="name" placeholder="Имя" required class="single-input"--}}
+{{--                                               @if(Auth::check()) value="{{\Illuminate\Support\Facades\Auth::user()->name}}" @endif >--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mt-10">--}}
+{{--                                        <input type="text" name="promo_code" placeholder="Промо-код" class="single-input">--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mt-10 left">--}}
+{{--                                        <input type="checkbox" id="public_offer" name="public_offer" required>--}}
+{{--                                        <label for="public_offer">Подтверждаю согласие с <a--}}
+{{--                                                href="{{route('public-offer')}}">Договором публичной оферты</a></label>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mt-10">--}}
+{{--                                        <input type="checkbox" id="privacy_policy" name="privacy_policy" required>--}}
+{{--                                        <label for="privacy_policy">Подтверждаю согласие на обработку <a--}}
+{{--                                                href="{{route('privacy')}}">персональных данных</a></label>--}}
+{{--                                    </div>--}}
 {{--                                    <div class="mt-10">--}}
 {{--                                        <button type="submit" class="btn_3">купить</button>--}}
 {{--                                    </div>--}}
-                                </div>
-                            </form>
+{{--                                </div>--}}
+{{--                            </form>--}}
                         @endif
                     </div>
                 </div>
